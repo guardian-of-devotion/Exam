1cd2da61f25afe2cad259bae7bc8751453b5de46
KittenAttack <guardian-of-devotion@yandex.ru>
2021-01-23 12:21:55 +0300

    Initial Commit

---
+++ Exam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exam
+//
+//  Created by Саркис Катвалян on 23/01/2021.
+//  Copyright © 2021 Саркис Катвалян. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
